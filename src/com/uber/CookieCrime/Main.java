99ecd06112f596f97ebae8e3aba92290d100f4d4
dg91 <dragos.godeanu@live.co.uk>
2014-10-15 20:03:50 +0300

    unfinished JSON parser
    Am suprascris codul tau si am inceput sa fac requestul pentru JSON.
    Trebuie sa vedem ce date vrem ca utilizatorul sa introduca pentru a stii
    ce requesturi urmeaza sa facem. Inca nu e complet finalizat, teoretic as
    mai avea putin.

--- src/com/uber/CookieCrime/Main.java
+++ src/com/uber/CookieCrime/Main.java
@@ -7,7 +7,8 @@ import android.view.View;
 import android.widget.EditText;
 
 public class Main extends Activity {
-    public final static String EXTRA_MESSAGE = "com.uber.CookieCrime.MESSAGE";
+    //public final static String EXTRA_MESSAGE = "com.uber.CookieCrime.MESSAGE";
+    public final static String URL = "http://data.police.uk/api/crimes-at-location?date=2012-02&location_id=54312";
     /**
      * Called when the activity is first created.
      */
@@ -15,14 +16,13 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
     }
 
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(URL, message);
         startActivity(intent);
     }
 
